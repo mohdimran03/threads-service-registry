7134ccd2da072a4c2ec1ec5a14fbe81d55863968
imran <imran@imrans-MacBook-Air.local>
2024-07-23 19:56:18 +0530

    initial commit

---
+++ src/test/java/com/example/threads_service_registry/ThreadsServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threads_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadsServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
